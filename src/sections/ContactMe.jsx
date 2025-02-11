1be93f79c662d45b0c712defa3d2265ebd59a0a1
Dhaval Patil <d22it209@charusat.edu.in>
2025-02-11 21:02:00 +0530

    email feature

--- src/sections/ContactMe.jsx
+++ src/sections/ContactMe.jsx
@@ -22,11 +22,18 @@ const ContactMe = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const emailData = {
+      to_name: "Dhaval", // Replace with dynamic name if needed
+      from_name: formData.name,
+      from_email: formData.email,
+      message: formData.message,
+    };
+
     emailjs
       .send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID, // Service ID
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID, // Template ID
-        formData,
+        emailData,
         import.meta.env.VITE_EMAILJS_USER_ID // Public Key
       )
       .then(
